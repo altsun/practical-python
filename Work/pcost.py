7935ac71c1b416cf79d6cca465d9db601d0ca88d
altsun <duongphamhn97@gmail.com>
2021-08-15 21:02:22 +0700

    Update pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,12 +1,24 @@
 # pcost.py
 #
 # Exercise 1.27
-stock_cost = 0.0
-with open('Data/portfolio.csv', 'r') as f:
-    headers = next(f)
-    for line in f:
-        stock_data = line.strip().split(',')
-        num_share = int(stock_data[1])
-        stock_price = float(stock_data[2])
-        stock_cost += num_share * stock_price
-print('Total cost:', stock_cost)
+import sys
+import csv
+
+def portfolio_cost(filename):
+    stock_cost = 0.0
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            num_share = int(row[1])
+            stock_price = float(row[2])
+            stock_cost += num_share * stock_price
+    return stock_cost
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
